6da4d6a95cc357be12b666237bdf51098a359460
Jayraj Singh <jayrajmthakur@gmail.com>
2026-04-21 23:23:14 +0530

    corsOptions added for render backend url

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 connectDB();
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "https://hukoom-web.onrender.com/"],
 };
 
 app.use(cors(corsOptions));

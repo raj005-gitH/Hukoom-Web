e3757e41ec56f3a50ae61f7c54a81e417b2cec50
Jayraj Singh <jayrajmthakur@gmail.com>
2026-04-21 20:23:28 +0530

    env file setup for deployment compmeted

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ const messageRoutes = require("./routes/messageRoutes");
 const cors = require("cors"); //cross-origin resource sharing
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
